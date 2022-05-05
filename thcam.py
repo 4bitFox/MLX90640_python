0cf83ce8df074430e879585bf3c1b19bdd4f7db1
4bitFox <33175205+4bitFox@users.noreply.github.com>
2022-05-05 15:28:50 +0200

    Update thcam.py

--- thcam.py
+++ thcam.py
@@ -4,8 +4,11 @@ import numpy as np
 import adafruit_mlx90640
 import matplotlib.backend_managers as bmg
 import matplotlib.pyplot as plt
+import datetime
 import keyboard
 import os
+
+
 
 TITLE = "Thermal Camera"
 SCREEN_W = 800
@@ -19,42 +22,62 @@ SPACE_T = 0.95
 color_bg = "black"
 color_fg = "white"
 interpolation = "kaiser" #none, nearest, bilinear, bicubic, spline16, spline36, hanning, hamming, hermite, kaiser, quadric, catrom, gaussian, bessel, mitchell, sinc, lanczos
-print_fps=True
+print_fps = True
+print_save = True
+
+SAVE_PREFIX = "THC_"
+SAVE_SUFFIX = ""
+SAVE_FILEFORMAT = "png"
 
 
-i2c = busio.I2C(board.SCL, board.SDA, frequency=800000) #setup I2C
-mlx = adafruit_mlx90640.MLX90640(i2c) #begin MLX90640 with I2C comm
-mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ #1, 2, 4, 8, 16, 32, 64 HZ
-MLX_SHAPE = (24, 32)
+
+#Init MLX
+i2c = busio.I2C(board.SCL, board.SDA, frequency=800000) #I2C
+mlx = adafruit_mlx90640.MLX90640(i2c) #MLX90640
+mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ #1, 2, 4, 8, 16, 32, 64 HZ possible
+MLX_SHAPE = (24, 32) #MLX resolution
 
 
-#setup the figure for plotting
-plt.ion() #enables interactive plotting
-fig,ax = plt.subplots(figsize=(12, 7))
+
+#Matplotlib
+plt.ion() #Interactive plotting
+fig,ax = plt.subplots(figsize=(12, 7)) #Figures & Axes
 therm1 = ax.imshow(np.zeros(MLX_SHAPE), vmin=0, vmax=60, interpolation=interpolation) #start plot with zeros
-
-
 
 fig.canvas.manager.set_window_title(TITLE) #Window title
 fig.canvas.manager.window.move(WINDOW_POS_X, WINDOW_POS_Y) #Move window
 fig.canvas.manager.window.resize(SCREEN_W, SCREEN_H) #Resize to fit screen
 fig.canvas.manager.toolbar.hide() #Hide toolbar
+fig.subplots_adjust(left=SPACE_L, bottom=SPACE_B, right=SPACE_R, top=SPACE_T) #Adjust space to border
 #fig.canvas.manager.full_screen_toggle() #Fullscreen
-fig.subplots_adjust(left=SPACE_L, bottom=SPACE_B, right=SPACE_R, top=SPACE_T)
 
+fig.patch.set_facecolor(color_bg) #Background color
 
-fig.patch.set_facecolor(color_bg)
 plt.xticks([]) #Hide xticks
 plt.yticks([]) #Hide yticks
 
 #Define temperature bar
-cbar = fig.colorbar(therm1) #setup colorbar for temps
-cbar.ax.yaxis.set_tick_params(color=color_fg)
-cbar.set_label('Temperature [$^{\circ}$C]', fontsize=14, color=color_fg) #colorbar label
-plt.setp(plt.getp(cbar.ax.axes, 'yticklabels'), color=color_fg)
+cbar = fig.colorbar(therm1) #Colorbar for temps
+cbar.ax.yaxis.set_tick_params(color=color_fg) #Tick color
+cbar.set_label('Temperature [$^{\circ}$C]', fontsize=14, color=color_fg) #Label
+plt.setp(plt.getp(cbar.ax.axes, 'yticklabels'), color=color_fg) #Tick labels
+
+
 
 frame = np.zeros((24*32, )) #setup array for storing all 768 temperatures
 t_array = []
+
+
+
+def datetime():
+    dt = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    return dt
+
+def save():
+    filename = SAVE_PREFIX + datetime() + SAVE_SUFFIX + "." + SAVE_FILEFORMAT
+    fig.savefig(filename)
+    if print_save:
+        print(filename)
 
 
 
@@ -81,5 +104,7 @@ def loop():
                 
         except ValueError:
             continue # if error, just read again
+
+
 
 loop()
