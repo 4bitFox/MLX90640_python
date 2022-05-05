81ac7b2e85e2c75503bea21a4626ab6b29186ff6
4bitFox <33175205+4bitFox@users.noreply.github.com>
2022-05-05 13:08:09 +0200

    Update thcam.py

--- thcam.py
+++ thcam.py
@@ -13,13 +13,11 @@ SCREEN_H = 480
 interpolation = "kaiser" #none, nearest, bilinear, bicubic, spline16, spline36, hanning, hamming, hermite, kaiser, quadric, catrom, gaussian, bessel, mitchell, sinc, lanczos
 print_fps=True
 
-
-print("Initializing MLX90640")
 i2c = busio.I2C(board.SCL, board.SDA, frequency=800000) #setup I2C
 mlx = adafruit_mlx90640.MLX90640(i2c) #begin MLX90640 with I2C comm
 mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ #1, 2, 4, 8, 16, 32, 64 HZ
 mlx_shape = (24, 32)
-print("Initialized")
+
 
 #setup the figure for plotting
 plt.ion() #enables interactive plotting
@@ -29,10 +27,16 @@ therm1 = ax.imshow(np.zeros(mlx_shape), vmin=0, vmax=60, interpolation=interpola
 cbar = fig.colorbar(therm1) #setup colorbar for temps
 cbar.set_label('Temperature [$^{\circ}$C]', fontsize=14) #colorbar label
 
-fig.canvas.manager.set_window_title(TITLE)
-fig.canvas.manager.window.move(0, -16)
-fig.canvas.manager.window.resize(SCREEN_W, SCREEN_H)
-#fig.canvas.manager.full_screen_toggle()
+fig.canvas.manager.set_window_title(TITLE) #Window title
+fig.canvas.manager.window.move(0, -16) #Move to top left
+fig.canvas.manager.window.resize(SCREEN_W, SCREEN_H) #Resize to fit screen
+fig.canvas.manager.toolbar.hide() #Hide toolbar
+#fig.canvas.manager.full_screen_toggle() #Fullscreen
+
+
+plt.rcParams['toolbar'] = 'toolmanager'
+buttons_remove = ['forward', 'back', 'pan', 'zoom', 'home', 'help']
+
 
 frame = np.zeros((24*32, )) #setup array for storing all 768 temperatures
 t_array = []
@@ -47,6 +51,7 @@ def loop():
     print("Starting loop")
     while True:
         t1 = time.monotonic()
+        
         try:
             mlx.getFrame(frame) #read MLX temperatures into frame var
             data_array = (np.reshape(frame, mlx_shape)) #reshape to 24x32
@@ -57,11 +62,13 @@ def loop():
             plt.pause(0.001) #required
             #fig.savefig('mlx90640_test_fliplr.png',dpi=300,facecolor='#FCFCFC', bbox_inches='tight') #comment out to speed up
             t_array.append(time.monotonic()-t1)
+            
             if print_fps:
                 #os.system("clear")
                 print('Sample Rate: {0:2.1f}fps'.format(len(t_array)/np.sum(t_array)))
 #            if keyboard.is_pressed("q"):
 #                close()
+
         except ValueError:
             continue # if error, just read again
 
