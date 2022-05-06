a59743fd8bf1b95e5c5e4a3d9729db470e663470
4bitFox <33175205+4bitFox@users.noreply.github.com>
2022-05-06 10:55:14 +0200

    Update thcam.py

--- thcam.py
+++ thcam.py
@@ -4,14 +4,18 @@ import numpy as np
 import adafruit_mlx90640
 #import matplotlib.backend_managers as bmg
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Button
 #import datetime
 #import keyboard
 import os
 
 
 temp_range = True
-temp_range_min = 25
-temp_range_max = 300
+temp_range_min = 25 #-40 °C
+temp_range_max = 300 #300 °C
+
+emissivity = 0.95
+EMISSIVITY_BASELINE = 1
 
 
 TITLE = "Thermal Camera"
@@ -36,6 +40,11 @@ PRINT_FPS = True
 PRINT_SAVE = True
 PRINT_DEBUG = True
 PRINT_VALUEERROR = True
+
+
+
+#Calculate emissivity compensation
+e_comp = EMISSIVITY_BASELINE / emissivity
 
 
 
@@ -102,15 +111,19 @@ def loop():
         try:
             mlx.getFrame(frame) #read MLX temperatures into frame var
             data_array = frame
+            data_array *= e_comp
             temp_min = np.min(data_array)
             temp_max = np.max(data_array)
             if temp_range:
                 data_array = np.clip(data_array, temp_range_min, temp_range_max) #Clip temps
             data_array = np.reshape(data_array, MLX_SHAPE) #Reshape to 24x32
             data_array = np.fliplr(data_array) #Flip left to right
+            
             therm1.set_data(data_array)
             therm1.set_clim(vmin=np.min(data_array), vmax=np.max(data_array)) #set bounds
             cbar.update_normal(therm1) #update colorbar range
+            
+            #Temp overview
             if not temp_range or temp_min > temp_range_min and temp_max < temp_range_max:
                 plt.title(f"Max Temp: {np.max(data_array):.1f} °C    Avg Temp: {np.average(data_array):.1f} °C    Min Temp: {np.min(data_array):.1f} °C", color=color_fg)
             elif temp_min < temp_range_min and temp_max < temp_range_max:
@@ -119,14 +132,17 @@ def loop():
                 plt.title(f"*Max Temp: > {np.max(data_array):.1f} °C  ({temp_max:.1f} °C)            Min Temp: {np.min(data_array):.1f} °C", color=color_fg)
             elif temp_min < temp_range_min and temp_max > temp_range_max:
                 plt.title(f"*Max Temp: > {np.max(data_array):.1f} °C  ({temp_max:.1f} °C)        *Min Temp: < {np.min(data_array):.1f} °C  ({temp_min:.1f} °C)", color=color_fg)
+                
             plt.pause(0.001) #required
             t_array.append(time.monotonic()-t1)
             
             if PRINT_FPS:
                 print("Sample Rate: {0:2.1f}fps".format(len(t_array)/np.sum(t_array)))
+                
         except ValueError:
             if PRINT_VALUEERROR:
                 print("ValueError")
+                
             continue # if error, just read again
 
 
