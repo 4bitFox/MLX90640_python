0dbe5359201bfe28f0e4300cdb2bfa87e20a3e43
4bitFox <33175205+4bitFox@users.noreply.github.com>
2022-06-02 15:22:06 +0200

    Update conf_new.py

--- conf_new.py
+++ conf_new.py
@@ -36,8 +36,11 @@ config_file.set("Temperature_Range", "range_max", "300")
 
 config_file.add_section("Monitor")
 config_file.set("Monitor", "monitor_pixels_enable", "False")
+config_file.set("Monitor", "monitor_buzzer_enable", "True")
 config_file.set("Monitor", "monitor_pixels_array", "[[0, 0, 20, 50], [0, 31, 30, 40], [23, 0, 10, 20], [23, 31, -10, 0], [11, 15, -40, 300]]")
-
+config_file.set("Monitor", "monitor_autotrigger_enable", "False")
+config_file.set("Monitor", "monitor_autotrigger_previous_frame", "5")
+config_file.set("Monitor", "monitor_autotrigger_array", "[[0, 0, 10, 50], [0, 31, 10, 50], [23, 0, 35, 50], [23, 31, 10, 50], [11, 15, 10, 50]]")
 
 config_file.add_section("Save")
 config_file.set("Save", "save_path", "/home/pi/thcam/saves/default")
