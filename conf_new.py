ef6921ab4323725c477b701adb0b33a0a5d70718
4bitFox <33175205+4bitFox@users.noreply.github.com>
2022-06-10 11:36:36 +0200

    Add comments

--- conf_new.py
+++ conf_new.py
@@ -21,7 +21,7 @@ config_file = configparser.ConfigParser()
 
 
 config_file.add_section("View")
-config_file.set("View", "interpolation", "gaussian")
+config_file.set("View", "interpolation", "gaussian #none, nearest, bilinear, bicubic, spline16, spline36, hanning, hamming, hermite, kaiser, quadric, catrom, gaussian, bessel, mitchell, sinc, lanczos")
 
 
 config_file.add_section("Accuracy")
@@ -35,12 +35,12 @@ config_file.set("Temperature_Range", "range_max", "300")
 
 
 config_file.add_section("Monitor")
-config_file.set("Monitor", "monitor_pixels_enable", "False")
-config_file.set("Monitor", "monitor_buzzer_enable", "True")
-config_file.set("Monitor", "monitor_pixels_array", "[[0, 0, 20, 50], [0, 31, 30, 40], [23, 0, 10, 20], [23, 31, -10, 0], [11, 15, -40, 300]]")
-config_file.set("Monitor", "monitor_autotrigger_enable", "False")
-config_file.set("Monitor", "monitor_autotrigger_previous_frame", "5")
-config_file.set("Monitor", "monitor_autotrigger_array", "[[0, 0, 10, 50], [0, 31, 10, 50], [23, 0, 35, 50], [23, 31, 10, 50], [11, 15, 10, 50]]")
+config_file.set("Monitor", "monitor_pixels_enable", "False #Monitor pixels, if they are in tolerance. If not, turn screen red.")
+config_file.set("Monitor", "monitor_buzzer_enable", "True #Buzz once if monitored pixels are not in temperature tolerance.")
+config_file.set("Monitor", "monitor_pixels_array", "[[0, 0, 20, 50], [0, 31, 30, 40], [23, 0, 10, 20], [23, 31, -10, 0], [11, 15, -40, 300]] #[[pos_y, pos_x, min_°C, max_°C], [...], ...]")
+config_file.set("Monitor", "monitor_autotrigger_enable", "False #A picture will be taken, if all pixels of 'monitor_autotrigger_array' are in temperature tolerance.")
+config_file.set("Monitor", "monitor_autotrigger_previous_frame", "5 #If a picture should automatically be taken, store a previous frame. For the current frame, set to '0'.")
+config_file.set("Monitor", "monitor_autotrigger_array", "[[0, 0, 10, 50], [0, 31, 10, 50], [23, 0, 35, 50], [23, 31, 10, 50], [11, 15, 10, 50]] #[[pos_y, pos_x, min_°C, max_°C], [...], ...]")
 
 config_file.add_section("Save")
 config_file.set("Save", "save_path", "/home/pi/thcam/saves/default")
