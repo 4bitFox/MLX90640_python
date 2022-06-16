636a7a0c42906ced6067d4ea7b0640e91e48e3d6
4bitFox <33175205+4bitFox@users.noreply.github.com>
2022-06-16 12:00:30 +0200

    Update read_thcam-dependencies.bat

--- read_thcam-dependencies.bat
+++ read_thcam-dependencies.bat
@@ -1,23 +1,25 @@
 @echo off 
 
-echo BITTE INSTALLIEREN SIE DIE NEUSTE VERSION VON "PYTHON 3" AUS DEM MICROSOFT STORE.
-echo Ich oeffne in 15 Sekunden den Store fuer Sie. :-)
+echo PLEASE INSTALL THE NEWEST VERSION OF PYTHON 3 FROM THE MICROSOFT STORE.
+echo I will open the Store for you in 15 seconds. :-)
 timeout /T 15
 explorer "ms-windows-store://publisher/?name=Python Software Foundation"
 
 echo ------------------------------------------------------------
-echo Wenn Sie Python 3 installiert haben, duerfen Sie fortfahren.
+echo When you have installed Python 3, you can continue...
 pause
 
 echo ------------------------------------------------------------
-echo Ich installiere nun die noetigen Python Bibliotheken:
+echo I will now install the required Python libraries for you:
 echo matplotlib configparser numpy
 echo ...
 pip3 install --upgrade pip
 pip3 install matplotlib configparser numpy
 
 echo ------------------------------------------------------------
-echo Alles erledigt.
-echo Du kannst nun ".thcam" Dateien mit "read_thcam.bat" oeffnen.
-echo Beim ersten mal muessen Sie die ".thcam" Dateien mit "Oeffnen mit" ausfuehren. Verweisen Sie dabei auf "read_thcam.bat".
+echo All done.
+echo You can open ".thcam" files with "read_thcam.bat".
+echo You have to enter the correct "cd" path into "read_thcam.bat" for it to work!
+echo The first time, you will have to use "open with" and point to "read_thcam.bat". 
+echo While you are at it, feel free to check the "always open with" checkbox.
 timeout /T 60
